81047666348b992d4876ae321b92807f9482bf90
Cláudio Santos <claudiosantos@sistnet.com.br>
2022-10-12 10:40:30 -0300

    cap 02 - variaveis

---
+++ 02_variaveis/variaveis.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "variavel 1"
+	variavel2 := "variavel 2"
+
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+
+	var (
+		variavel3 string = "variavel3"
+		variavel4 string = "variavel4"
+	)
+
+	fmt.Println(variavel3, variavel4)
+
+	variavel5, variavel6 := "variavel5", "variavel6"
+
+	fmt.Println(variavel5, variavel6)
+}
