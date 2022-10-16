fb0a5cb031b44585efa4f24ecb3544f01cbf8f6a
Cláudio Santos <claudiosantos@sistnet.com.br>
2022-10-16 17:52:00 -0300

    cap 16 - app linha de comando - busca de nome de servidor

--- 16_app_linha_comando/app/app.go
+++ 16_app_linha_comando/app/app.go
@@ -13,17 +13,25 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação linha de Comando"
 	app.Usage = "Pega IP e Nome de Servidores na Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "sistnet.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Bus ca IPs de endereços na Internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "sistnet.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Bus ca IPs de endereços na Internet",
+			Flags:  flags,
 			Action: pegarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Pega o nome do servidor na Internet",
+			Flags:  flags,
+			Action: pegarServidores,
 		},
 	}
 
@@ -41,5 +49,19 @@ func pegarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func pegarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	server, erro := net.LookupNS(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range server {
+		fmt.Println(servidor.Host)
 	}
 }
