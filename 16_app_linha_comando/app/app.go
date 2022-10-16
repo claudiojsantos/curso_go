d991509027680ae6ace1446a06c2b6ae672d2458
Cláudio Santos <claudiosantos@sistnet.com.br>
2022-10-16 17:24:23 -0300

    cap 16 - app linha de comando - busca de ip

--- 16_app_linha_comando/app/app.go
+++ 16_app_linha_comando/app/app.go
@@ -1,11 +1,45 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação linha de Comando"
 	app.Usage = "Pega IP e Nome de Servidores na Internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Bus ca IPs de endereços na Internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "sistnet.com.br",
+				},
+			},
+			Action: pegarIps,
+		},
+	}
+
 	return app
+}
+
+func pegarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
